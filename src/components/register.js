62a4bbd03bf41e5f7fddb2811f4db75dc03d9ce4
Jayrpatel2 <jayrpatel211101@gmail.com>
2024-02-04 18:52:36 -0500

    register

--- src/components/register.js
+++ src/components/register.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import PageHeader from './global-components/page-header';
-import Register from './section-components/register';
-import CallToActionV1 from './section-components/call-to-action-v1';
-import Footer from './global-components/footer';
-import NavbarV2 from './global-components/navbar-v2';
+import React from "react";
+import PageHeader from "./global-components/page-header";
+import Register from "./section-components/register";
+import CallToActionV1 from "./section-components/call-to-action-v1";
+import Footer from "./global-components/footer";
+import NavbarV2 from "./global-components/navbar-v2";
 
 const RegisterV1 = () => {
-    return <div>
-        <NavbarV2 />
-        <PageHeader headertitle="Account" subheader="Register" />
-        <Register />
-        <CallToActionV1 />
-        <Footer />
+  return (
+    <div>
+      <NavbarV2 />
+      <PageHeader headertitle="Account" subheader="Register" />
+      <Register />
+      <Footer />
     </div>
-}
+  );
+};
 
-export default RegisterV1
-
+export default RegisterV1;
