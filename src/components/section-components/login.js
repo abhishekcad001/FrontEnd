60ebe2475b11cd24e76d4ba4f2460cb1eeb52698
harsh-a11 <hspatel791999@gmail.com>
2024-02-06 09:20:18 -0500

    model for forget pass

--- src/components/section-components/login.js
+++ src/components/section-components/login.js
@@ -47,7 +47,46 @@ class Login extends Component {
 				</div>
 				</div>
 						</div>
-			
+			<div className="ltn__modal-area ltn__add-to-cart-modal-area----">
+			<div className="modal fade" id="ltn_forget_password_modal" tabIndex={-1}>
+				<div className="modal-dialog modal-md" role="document">
+				<div className="modal-content">
+					<div className="modal-header">
+					<button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">×</span>
+					</button>
+					</div>
+					<div className="modal-body">
+					<div className="ltn__quick-view-modal-inner">
+						<div className="modal-product-item">
+						<div className="row">
+							<div className="col-12">
+							<div className="modal-product-info text-center">
+								<h4>FORGET PASSWORD?</h4>
+								<p className="added-cart"> Enter you register email.</p>
+								<form action="#" className="ltn__form-box">
+								<input type="text" name="email" placeholder="Type your register email*" />
+								<div className="btn-wrapper mt-0">
+									<button className="theme-btn-1 btn btn-full-width-2" type="submit">Submit</button>
+								</div>
+								</form>
+							</div>
+					
+							<div className="additional-info d-none">
+								<p>We want to give you <b>10% discount</b> for your first order, <br />  Use discount code at checkout</p>
+								<div className="payment-method">
+								<img src={publicUrl+"assets/img/icons/payment.png"} alt="#" />
+								</div>
+							</div>
+							</div>
+						</div>
+						</div>
+					</div>
+					</div>
+				</div>
+				</div>
+			</div>
+			</div>
 	</>
         }
 }
